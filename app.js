74e343573f831255f79d6bca4bebd7d41ca7190c
evajacuk <jacukevangelina@gmail.com>
2022-07-06 22:05:03 -0300

    inserta Mensaje aquí

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express= require ('express');
+
 const app = express (); 
+
 app.use(express.static('public'));
 
 app.set('puerto', process.env.PORT || 3030);
 
+app.listen (app.get('puerto'), () => console.log 
+('Servidor escuchando en puerto 
+${app.get ('puerto')}')) ;
 
-app.listen (3030,()=>{console.log('Servidor escuchando');});
+(process.env.PORT|| 3030, function() 
+{console.log ('Servidor escuchando')});
+
+//app.listen (3030,()=>{ console.log ('Servidor escuchando');});
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
